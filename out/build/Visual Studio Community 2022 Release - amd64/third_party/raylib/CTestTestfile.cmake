0a5984b40167b18487a6d4cf0d8b12f253b2cbe7
Babiciu Iustin <167506994+justy41@users.noreply.github.com>
2025-09-17 21:33:40 +0300

    ignored out/build folder

--- out/build/Visual Studio Community 2022 Release - amd64/third_party/raylib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Iustin/Documents/PROJECTS/_C++/component_system/third_party/raylib
-# Build directory: C:/Users/Iustin/Documents/PROJECTS/_C++/component_system/out/build/Visual Studio Community 2022 Release - amd64/third_party/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("raylib")
